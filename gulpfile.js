b3b17a36cc30ca99d64f25cb57d2194b3c4121e4
Rudenko Artem <artem.rud@bk.ru>
2019-07-03 10:09:34 +0300

    add gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const gulpStylelint = require('gulp-stylelint');
 
-function style (){
+function style() {
     return gulp.src('./scss/style.scss')
-            .pipe(sass())
-            .pipe(gulp.dest('./css'))
-            .pipe(browserSync.stream())
+        .pipe(sass())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream())
 }
 
 
-function watch () {
+function lintCss() {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(gulpStylelint({
+            reporters: [
+                {
+                    formatter: 'string',
+                    console: true
+              }
+      ]
+        }));
+}
+
+function watch() {
     browserSync.init({
 
         server: {
@@ -21,7 +34,8 @@ function watch () {
     gulp.watch('./*.html').on('change', browserSync.reload);
 }
 
-exports.style = style;
-exports.watch = watch;
 
+exports.style = style;
+exports.lintCss = lintCss;
+exports.watch = watch;
 
